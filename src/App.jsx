1e0f31993ab1b56be9c7de06a45530d13cac6dea
macikb8@yahoo.com <macikb8@yahoo.com>
2024-03-04 22:50:56 +0100

    add search component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
+import List from './components/list/List'
 import Search from './components/search/Search'
 
 function App() {
 
   return (
-    <>
+    <div className='app'>
       <h1>Todos</h1>
       <Search />
-    </>
+      <List />
+    </div>
   )
 }
 
